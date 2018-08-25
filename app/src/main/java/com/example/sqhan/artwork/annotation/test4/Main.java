998d4ee71d93cba83f4bc60479ae182302426558
Your Name <you@example.com>
2018-08-25 22:15:07 +0800

    list addAll() test

--- app/src/main/java/com/example/sqhan/artwork/annotation/test4/Main.java
+++ app/src/main/java/com/example/sqhan/artwork/annotation/test4/Main.java
@@ -1,5 +1,8 @@
 package com.example.sqhan.artwork.annotation.test4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by hanshenquan.
  * 反射注解Field字段注入值
@@ -9,6 +12,22 @@ public class Main {
     public static void main(String[] args) {
         MyClassA a = new MyClassA();
         a.getV();
+
+        List<String> x1 = new ArrayList<>();
+        x1.add("1");
+        x1.add("2");
+
+        List<String> x2 = new ArrayList<>();
+        x2.add("3");
+        x2.add("4");
+        x2.add("5");
+
+//        x1.clear();
+        x1.addAll(x2); // [1, 2, 3, 4, 5]
+//        x1.addAll(0, x2); // [3, 4, 5, 1, 2]
+//        x1.addAll(1, x2); // [1, 3, 4, 5, 2]
+
+        System.out.println(x1.toString());
     }
 
     private static class MyClass {
