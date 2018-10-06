b351fcc61eade94c0dbb0a127276fd473fcc733d
Your Name <you@example.com>
2018-10-06 20:15:49 +0800

    md5 test

---
+++ javamodule/src/main/java/com/example/javamodule/Md5Test.java
@@ -0,0 +1,47 @@
+package com.example.javamodule;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by hanshenquan.
+ * md5随便测试
+ */
+public class Md5Test {
+    public static void main(String[] args) {
+        String x = "话剧1";
+//        String x = "电子书1";
+        String result = md5(md5(x) + x);
+        System.out.println(result);
+        System.out.println(result.substring(0,10));
+
+    }
+
+
+    public final static String md5(String s) {
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        try {
+            byte[] btInput = s.getBytes();
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+
